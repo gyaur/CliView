1ffd2077ff5ad9c48c7853dbaf0f088155a43be7
Bálint Gyarmathy <bulint1@gmail.com>
2020-10-18 22:08:08 +0200

    Add shared library

--- api/src/lib/lib.rs
+++ api/src/lib/lib.rs
@@ -1,5 +1,7 @@
 mod config;
 mod types;
+mod video_processing;
 
 pub use config::*;
 pub use types::*;
+pub use video_processing::*;
--- api/src/lib/types.rs
+++ api/src/lib/types.rs
@@ -1,8 +1,42 @@
+use serde::{Deserialize, Serialize};
+use std::{collections::VecDeque, sync::Mutex};
+
 pub type Error = Box<dyn std::error::Error>;
 pub type GenericResult<T> = Result<T, Error>;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Url {
+    pub url: String,
+}
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Volume {
+    pub volume: i32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Ammount {
+    pub ammount: i32,
+}
+
 #[derive(Debug, Clone)]
 pub enum Event {
+    Stream(Url),
     Skip,
     VolumeUp,
+    VolumeDown,
+    VolumeSet(Volume),
+    Seek(Ammount),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueueState {
+    pub queue: Mutex<VecDeque<Url>>,
+}
+
+impl QueueState {
+    pub fn new() -> Self {
+        Self {
+            queue: Mutex::from(VecDeque::new()),
+        }
+    }
 }
