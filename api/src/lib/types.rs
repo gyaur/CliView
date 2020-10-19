3b0255be0a177c7b9a4ca1185ec949033a93f397
Bálint Gyarmathy <bulint1@gmail.com>
2020-10-19 19:52:38 +0200

    Add new impl for Url,Volume and Ammount. Add QueueStateSendable

--- api/src/lib/types.rs
+++ api/src/lib/types.rs
@@ -8,14 +8,33 @@ pub type GenericResult<T> = Result<T, Error>;
 pub struct Url {
     pub url: String,
 }
+
+impl Url {
+    pub fn new(url: String) -> Self {
+        Self { url }
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Volume {
     pub volume: i32,
 }
 
+impl Volume {
+    pub fn new(volume: i32) -> Self {
+        Self { volume }
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Ammount {
     pub ammount: i32,
+}
+
+impl Ammount {
+    pub fn new(ammount: i32) -> Self {
+        Self { ammount }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -33,10 +52,26 @@ pub struct QueueState {
     pub queue: Mutex<VecDeque<Url>>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct QueueStateSendable {
+    pub queue: Vec<Url>,
+}
+
+impl QueueStateSendable {
+    pub fn new(queue: Vec<Url>) -> Self {
+        Self { queue }
+    }
+}
+
 impl QueueState {
     pub fn new() -> Self {
         Self {
             queue: Mutex::from(VecDeque::new()),
         }
+    }
+    pub fn to_response(&self) -> QueueStateSendable {
+        let queue = self.queue.lock().unwrap();
+
+        QueueStateSendable::new(queue.iter().map(|x| x.to_owned()).collect::<Vec<Url>>())
     }
 }
