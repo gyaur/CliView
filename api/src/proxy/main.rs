b6bfb745a046429304066267393a600b31c49043
Bálint Gyarmathy <bulint1@gmail.com>
2020-10-19 19:53:38 +0200

    Add proxy functionality to one of the requests

--- api/src/proxy/main.rs
+++ api/src/proxy/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(proc_macro_hygiene, decl_macro)]
+use lib::Config as CliViewConfig;
+use lib::GenericResult as Result;
+use rocket;
+use rocket::config::{Config, Environment};
+use rocket::response::Redirect;
+
+#[rocket::get("/queue")]
+fn queue_get() -> Redirect {
+    Redirect::to(format!("http://localhost:{}/queue", 5001))
+}
+//TODO: Write macros to do this for every function
+
+fn main() -> Result<()> {
+    let cfg = CliViewConfig::load()?;
+    let rocket_config = Config::build(Environment::Staging)
+        .address("127.0.0.1")
+        .port(cfg.proxy_port)
+        .workers(cfg.num_workers)
+        .unwrap();
+
+    rocket::custom(rocket_config)
+        .mount("/", rocket::routes![queue_get])
+        .launch();
+
+    Ok(())
 }
