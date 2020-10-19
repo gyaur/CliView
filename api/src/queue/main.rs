b861c4ca795597365ede8a16810a9718f066dffb
Bálint Gyarmathy <bulint1@gmail.com>
2020-10-19 19:54:07 +0200

    Add basic queueing functionality

--- api/src/queue/main.rs
+++ api/src/queue/main.rs
@@ -1,11 +1,12 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-use lib::extract_url;
 use lib::Config as CliViewConfig;
 use lib::GenericResult as Result;
 use lib::QueueState;
 use lib::Url;
+use lib::{extract_url, QueueStateSendable};
 use rocket;
 use rocket::config::{Config, Environment};
+use rocket::http::Status;
 use rocket::State;
 use rocket_contrib::json::Json;
 
@@ -15,6 +16,23 @@ fn front(state: State<QueueState>) -> Json<Option<Url>> {
     Json(queue.pop_front())
 }
 
+#[rocket::get("/queue")]
+fn queue_get(state: State<QueueState>) -> Json<QueueStateSendable> {
+    Json(state.to_response())
+}
+
+#[rocket::post("/queue", data = "<data>")]
+fn queue_post(state: State<QueueState>, data: Json<Url>) -> Status {
+    let res = extract_url(&data.0);
+    match res {
+        Ok(url) => {
+            let mut queue = state.queue.lock().unwrap();
+            queue.push_back(url);
+            Status::Accepted
+        }
+        Err(_) => Status::BadRequest,
+    }
+}
 fn main() -> Result<()> {
     let cfg = CliViewConfig::load()?;
     let rocket_config = Config::build(Environment::Staging)
@@ -23,16 +41,9 @@ fn main() -> Result<()> {
         .workers(cfg.num_workers)
         .unwrap();
 
-    let state = QueueState::new();
-    {
-        let mut queue = state.queue.lock().unwrap();
-        queue.push_back(Url {
-            url: extract_url("https://www.youtube.com/watch?v=9em32dDnTck")?,
-        });
-    }
     rocket::custom(rocket_config)
-        .mount("/", rocket::routes![front])
-        .manage(state)
+        .mount("/", rocket::routes![front, queue_get, queue_post])
+        .manage(QueueState::new())
         .launch();
 
     Ok(())
