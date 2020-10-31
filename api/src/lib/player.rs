ab1f537333d2ac57d0d74ba2947078a80afbe041
Bálint Gyarmathy <bulint1@gmail.com>
2020-10-31 22:24:27 +0100

    Add player trait

---
+++ api/src/lib/player.rs
@@ -0,0 +1,11 @@
+pub trait Player {
+    fn start(process: Popen, media: Url, volume: Volume);
+    fn stop(process: Popen);
+    fn play(process: Popen);
+    fn pause(process: Popen);
+    fn skip(process: Popen);
+    fn seek(process: Popen, ammount: Ammount);
+    fn increase_volume(process: Popen);
+    fn decrease_volume(process: Popen);
+    fn set_volume(process: Popen, volume: Volume);
+}
