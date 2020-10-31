c68a84d2f258b4f023e5e876dcc4003c7eba8ae8
Bálint Gyarmathy <bulint1@gmail.com>
2020-10-31 22:22:21 +0100

    Add player trait for abstracting the media player

--- api/src/lib/lib.rs
+++ api/src/lib/lib.rs
@@ -1,9 +1,11 @@
 mod config;
 mod macros;
+mod player;
 mod types;
 mod video_processing;
 
 pub use config::*;
 pub use macros::*;
+pub use player::*;
 pub use types::*;
 pub use video_processing::*;
