679f81e6361a8b16fef5451e0bf69d509e3a5e6a
Bálint Gyarmathy <bulint1@gmail.com>
2020-11-03 21:15:37 +0100

    Refactor proxy service and add tests

--- api/src/proxy/main.rs
+++ api/src/proxy/main.rs
@@ -2,23 +2,27 @@
 use lib::gen_proxy_function;
 use lib::Config as CliViewConfig;
 use lib::GenericResult as Result;
-use rocket::config::{Config, Environment};
 use rocket::response::Redirect;
+use rocket::State;
+use rocket::{
+    config::{Config, Environment},
+    Rocket,
+};
 
 //Queue service functions
-gen_proxy_function!(queue_get, "/queue", get, 5001);
-gen_proxy_function!(queue_post, "/queue", post, 5001);
+gen_proxy_function!(queue_get, "/queue", get, queue_port);
+gen_proxy_function!(queue_post, "/queue", post, queue_port);
 
 //Command service functions
-gen_proxy_function!(stream, "/stream", post, 5003);
-gen_proxy_function!(increase_volume, "/inc", post, 5003);
-gen_proxy_function!(lower_volume, "/dec", post, 5003);
-gen_proxy_function!(set_volume, "/volume", post, 5003);
-gen_proxy_function!(get_volume, "/volume", get, 5003);
-gen_proxy_function!(seek, "/seek", post, 5003);
-gen_proxy_function!(skip, "/skip", post, 5003);
+gen_proxy_function!(stream, "/stream", post, command_port);
+gen_proxy_function!(increase_volume, "/inc", post, command_port);
+gen_proxy_function!(lower_volume, "/dec", post, command_port);
+gen_proxy_function!(set_volume, "/volume", post, command_port);
+gen_proxy_function!(get_volume, "/volume", get, command_port);
+gen_proxy_function!(seek, "/seek", post, command_port);
+gen_proxy_function!(skip, "/skip", post, command_port);
 
-fn main() -> Result<()> {
+fn setup_rocket() -> Result<Rocket> {
     let cfg: CliViewConfig = CliViewConfig::load()?;
     let rocket_config = Config::build(Environment::Development)
         .address("0.0.0.0")
@@ -27,7 +31,7 @@ fn main() -> Result<()> {
         .finalize()
         .unwrap();
 
-    rocket::custom(rocket_config)
+    let rocket = rocket::custom(rocket_config)
         .mount(
             "/",
             rocket::routes![
@@ -42,7 +46,55 @@ fn main() -> Result<()> {
                 skip
             ],
         )
-        .launch();
+        .manage(cfg);
 
+    Ok(rocket)
+}
+
+fn main() -> Result<()> {
+    let rocket = setup_rocket()?;
+    rocket.launch();
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use lib::Config as CliViewConfig;
+    use rocket::{http::Status, local::Client};
+
+    use super::setup_rocket;
+
+    fn setup_rocket_test_client() -> Client {
+        Client::new(setup_rocket().unwrap()).expect("valid rocket instance")
+    }
+
+    macro_rules! generate_test {
+        ($name:ident,$port:ident,$method:ident,$uri:expr) => {
+            #[test]
+            fn $name() {
+                let cfg: CliViewConfig = CliViewConfig::load().unwrap();
+                let client = setup_rocket_test_client();
+                let response = client.$method($uri).dispatch();
+
+                assert_eq!(response.status(), Status::TemporaryRedirect);
+                assert!(
+                    response.headers().get("Location").nth(0).unwrap()
+                        == format!("http://localhost:{}{}", cfg.$port, $uri)
+                )
+            }
+        };
+    }
+
+    //Queue service tests
+    generate_test!(test_queue_get, queue_port, get, "/queue");
+    generate_test!(test_queue_post, queue_port, post, "/queue");
+
+    //Command service tests
+    generate_test!(test_stream, command_port, post, "/stream");
+    generate_test!(test_increase_volume, command_port, post, "/inc");
+    generate_test!(test_lower_volume, command_port, post, "/dec");
+    generate_test!(test_volume_post, command_port, post, "/volume");
+    generate_test!(test_volume_get, command_port, get, "/volume");
+    generate_test!(test_seek, command_port, post, "/seek");
+    generate_test!(test_skip, command_port, post, "/skip");
 }
