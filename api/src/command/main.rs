b580198f3b066d719d05119f16b497d855f69017
Bálint Gyarmathy <bulint1@gmail.com>
2020-11-06 17:43:19 +0100

    Add tests for command

--- api/src/command/main.rs
+++ api/src/command/main.rs
@@ -1,5 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-use lib::Url;
+use lib::{extract_url, Url};
 use lib::{Action, GenericResult as Result, Volume};
 use lib::{Ammount, PlaybackStatus};
 use lib::{CommandQueue, Config as CliViewConfig};
@@ -35,7 +35,8 @@ fn playback(state: State<CommandQueue>) -> Json<PlaybackStatus> {
 #[rocket::post("/stream", data = "<url>")]
 fn stream(state: State<CommandQueue>, url: Json<Url>) {
     let mut queue = state.queue.lock().unwrap();
-    queue.push_front(Action::Stream(url.0));
+    let url = extract_url(&url.0).unwrap();
+    queue.push_front(Action::Stream(url));
 }
 
 #[rocket::post("/inc")]
@@ -82,7 +83,7 @@ fn get_volume(state: State<CommandQueue>) -> Json<Volume> {
 #[rocket::post("/seek", data = "<seek>")]
 fn seek(state: State<CommandQueue>, seek: Json<Ammount>) -> Status {
     let mut queue = state.queue.lock().unwrap();
-    if (seek.ammount % 600) % 30 != 0 || seek.ammount <= 0 {
+    if (seek.ammount % 600) % 30 != 0 {
         return Status::BadRequest;
     }
     queue.push_back(Action::Seek(seek.0));
@@ -131,6 +132,184 @@ fn main() -> Result<()> {
 }
 
 #[cfg(test)]
-mod test{
-    
+mod test {
+    use crate::setup_rocket;
+    use crate::CliViewConfig;
+    use lib::{Action, PlaybackStatus, Volume};
+    use rocket::http::ContentType;
+    use rocket::http::Status;
+    use rocket::local::Client;
+
+    fn setup_rocket_test_client() -> Client {
+        let cfg = CliViewConfig::load().unwrap();
+        Client::new(setup_rocket(cfg)).expect("valid rocket instance")
+    }
+    #[test]
+    fn test_front() {
+        let client = setup_rocket_test_client();
+        let mut response = client.get("/front").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::JSON));
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+        assert!(action.is_none());
+
+        let response = client.post("/play").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let mut response = client.get("/front").dispatch();
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+        assert_eq!(action.unwrap(), Action::Play);
+    }
+
+    #[test]
+    fn test_stream() {
+        let client = setup_rocket_test_client();
+        let response = client.post("/pause").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let response = client
+            .post("/stream")
+            .body(r#"{"url":"https://www.youtube.com/watch?v=9em32dDnTck"}"#)
+            .dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let mut response = client.get("/front").dispatch();
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+
+        if let Some(Action::Stream(_)) = action {
+            assert!(true)
+        } else {
+            assert!(false)
+        }
+    }
+
+    #[test]
+    fn test_playback() {
+        let client = setup_rocket_test_client();
+        let mut response = client.get("/playback").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.content_type(), Some(ContentType::JSON));
+        let state =
+            serde_json::from_str::<PlaybackStatus>(&response.body_string().unwrap()).unwrap();
+        assert!(!state.status);
+    }
+
+    #[test]
+    fn test_inc() {
+        let client = setup_rocket_test_client();
+        let mut response = client.get("/volume").dispatch();
+        let mut base_volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+            .unwrap()
+            .volume;
+        for _ in 0..10 {
+            let response = client.post("/inc").dispatch();
+            assert_eq!(response.status(), Status::Ok);
+            let mut response = client.get("/volume").dispatch();
+            let new_volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+                .unwrap()
+                .volume;
+            assert!(new_volume == base_volume + 1);
+            base_volume = new_volume;
+        }
+        assert!(base_volume == 10);
+        let response = client.post("/inc").dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+
+    }
+
+    #[test]
+    fn test_dec() {
+        let client = setup_rocket_test_client();
+        let mut response = client.get("/volume").dispatch();
+        let base_volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+            .unwrap()
+            .volume;
+        assert!(base_volume == 0);
+        let response = client.post("/dec").dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+        let response = client.post("/inc").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let response = client.post("/dec").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let mut response = client.get("/volume").dispatch();
+        let new_volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+            .unwrap()
+            .volume;
+        assert!(new_volume == base_volume);
+    }
+
+    #[test]
+    fn test_set_volume() {
+        let client = setup_rocket_test_client();
+        let response = client.post("/volume").body(r#"{"volume": 5}"#).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let mut response = client.get("/volume").dispatch();
+        let volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+            .unwrap()
+            .volume;
+        assert!(volume == 5);
+        let response = client.post("/volume").body(r#"{"volume": 15}"#).dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+        let mut response = client.get("/volume").dispatch();
+        let volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+            .unwrap()
+            .volume;
+        assert!(volume == 5);
+        let response = client.post("/volume").body(r#"{"volume": -5}"#).dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+        let mut response = client.get("/volume").dispatch();
+        let volume = serde_json::from_str::<Volume>(&response.body_string().unwrap())
+            .unwrap()
+            .volume;
+        assert!(volume == 5)
+    }
+
+    #[test]
+    fn test_seek() {
+        let client = setup_rocket_test_client();
+        let response = client.post("/seek").body(r#"{"ammount": 30}"#).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let mut response = client.get("/front").dispatch();
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+        if let Some(Action::Seek(amm)) = action {
+            assert!(amm.ammount == 30);
+        } else {
+            assert!(false);
+        }
+        let client = setup_rocket_test_client();
+        let response = client.post("/seek").body(r#"{"ammount": -600}"#).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let mut response = client.get("/front").dispatch();
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+        if let Some(Action::Seek(amm)) = action {
+            assert!(amm.ammount == -600);
+        } else {
+            assert!(false);
+        }
+        let client = setup_rocket_test_client();
+        // incorrect ammount
+        let response = client.post("/seek").body(r#"{"ammount": -125}"#).dispatch();
+        assert_eq!(response.status(), Status::BadRequest);
+        let mut response = client.get("/front").dispatch();
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+        assert!(action.is_none());
+    }
+
+    #[test]
+    fn test_skip() {
+        let client = setup_rocket_test_client();
+        let response = client.post("/skip").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+
+        let mut response = client.get("/front").dispatch();
+        let action =
+            serde_json::from_str::<Option<Action>>(&response.body_string().unwrap()).unwrap();
+        assert_eq!(action.unwrap(), Action::Skip);
+    }
 }
